2bb311b7e8b854500461a9b1e684efaba682cfbc
Roger Rodrigues <rogervrodrigues@gmail.com>
2022-05-05 10:11:43 +0200

    Lab 03 :: First results

--- lab03/plot_experiment_metrics.py
+++ lab03/plot_experiment_metrics.py
@@ -4,9 +4,11 @@ from pathlib import Path
 from image_classification.data import load_logged_metrics, plot_metrics_lab03
 
 logs_list = [
-    ('Fixed lr=0.001', './logs/w3_logged_metrics_fixed_lr0001.csv'),
-    ('Fixed lr=0.001 (second exp.)', './logs/w3_logged_metrics_fixed_lr0001_exp2.csv'),
+    # ('Fixed lr=0.001', './logs/w3_logged_metrics_fixed_lr0001.csv'),
+    # ('Fixed lr=0.001 (second exp.)', './logs/w3_logged_metrics_fixed_lr0001_exp2.csv'),
     ('Scheduled lr', './logs/w3_logged_metrics.csv'),
+    ('Scheduled lr (Update=False)', './logs/w3_logged_metrics_lr_update_false.csv'),
+
 ]
 
 fig, axes, colors = None, None, None
@@ -20,7 +22,7 @@ for i, (label, log_dir) in enumerate(logs_list):
                                     logged_metrics,
                                     dir_fig='logs/w3_logged_metrics_compared.png',
                                     label=label,
-                                    fig = fig, 
+                                    fig = fig,
                                     axes = axes,
                                     colors = colors,
                                     plotfig=i == (len(logs_list) - 1),
